ae83b7adc82ae499ea46bc3a1285eb5d74dcf883
yeasin-hossain <yeasinxdp@gmail.com>
2024-05-25 00:51:51 +0600

    Create blog respository

--- Repositories/Entity/Blog.cs
+++ Repositories/Entity/Blog.cs
@@ -7,15 +7,15 @@ namespace oyster_blog.Repositories.Entity
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public int Id { get; set; }
+        public ObjectId Id { get; set; }
 
         public string Title { get; set; }
 
         public string Description { get; set; }
 
-        public DateTime CreatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
 
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
 
         // TODO add created by and updated by
     }
---
+++ Repositories/Interface/IBlogRepository.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using oyster_blog.Repositories.Entity;
+using oyster_blog.Service.DTO.Request;
+
+namespace oyster_blog.Repositories.Interface
+{
+    public interface IBlogRepository
+    {
+        Task<List<Blog>> GetBlogsAsync();
+
+        Task<Blog> GetBlogAsync(ObjectId blogId);
+
+        Task DeleteBlogAsync(ObjectId blogId);
+
+        Task<Blog?> CreateBlogAsync(Blog? blog);
+
+        Task<Blog> UpdateBlogAsync(ObjectId blogId, Blog blog);
+    }
+}
